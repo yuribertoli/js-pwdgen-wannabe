9f623e7245de401007d415d89473cfc32015c51e
yuribertoli <yuribertoli@yahoo.it>
2022-01-19 16:03:18 +0100

    family members

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ let chiave = nome + cognome + colore + "19";
 document.getElementById("password").innerHTML = "La tua password è: " + chiave;
 
 
-const actualYear = 2022;
+let actualYear = 2022;
 let yearOfBirthQuestion = prompt("In quale anno sei nato?");
 yearOfBirthQuestion = parseInt(yearOfBirthQuestion);
 let yourAge = actualYear - yearOfBirthQuestion;
 document.getElementById("età").innerHTML = "Hai " + yourAge + " anni vero?";
+
+
+const yourself = 1;
+let familyMembers = prompt("Da quante altre persone è composta la tua famiglia oltre a te?");
+familyMembers = parseInt(familyMembers);
+let totalFamily = yourself + familyMembers;
+document.getElementById("boh").innerHTML = `In famiglia siete ${totalFamily} persone`
+
+
 console.log(yourAge);
+console.log(totalFamily);
 
