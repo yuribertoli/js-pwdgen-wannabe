285518e0fb7bc33cce565ab37cf27b3bd5c4a9a2
yuribertoli <yuribertoli@yahoo.it>
2022-01-19 15:46:12 +0100

    anno di nascita

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const nome = prompt("Qual'è il tuo nome?");
-
 const cognome = prompt("Qual'è il tuo cognome?");
-
 const colore = prompt("Qual'è il tuo colore preferito?");
-
 let chiave = nome + cognome + colore + "19";
-
 document.getElementById("password").innerHTML = "La tua password è: " + chiave;
 
+
+const actualYear = 2022;
+let yearOfBirthQuestion = prompt("In quale anno sei nato?");
+yearOfBirthQuestion = parseInt(yearOfBirthQuestion);
+let yourAge = actualYear - yearOfBirthQuestion;
+document.getElementById("età").innerHTML = "Hai " + yourAge + " anni vero?";
+console.log(yourAge);
 
